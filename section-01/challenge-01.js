7a6eaff7561f3188d5bded60bc00c6c8ab40b808
terc.rafael.santos <terc.rafael.santos@saraiva.com.br>
2020-04-29 20:24:51 -0300

    challenge-01

---
+++ section-01/challenge-01.js
@@ -0,0 +1,38 @@
+var myvar;
+
+myvar = 10;
+
+var soma;
+
+soma = 15 + 8;
+
+soma ++;
+
+soma *= 3;
+
+console.log(soma);
+
+var souninja = true;
+
+var comida = ["arroz", "feijão", "ovo"];
+
+console.log(comida[1]);
+
+if(myvar === soma ) {
+	console.log('É igual');
+} else {
+	console.log('É diferente');
+}
+
+
+if(myvar <= soma ) {
+	console.log('menor ou igual');
+} else {
+	console.log('maior');
+}
+
+var divisao = function(a, b) {
+	return a/b;
+}
+
+console.log(divisao(10,2));
