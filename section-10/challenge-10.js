ba0c364332d1ea9c62e98e6014b48b237a47c330
Rafael Gomes <rafarfl2010@gmail.com>
2020-05-16 21:14:20 -0300

    continuing challenge 10

--- section-10/challenge-10.js
+++ section-10/challenge-10.js
@@ -10,6 +10,7 @@ e faça a indentação correta.
 	resolver o problema corretamente.
 	*/
 	var five = +'5';
+
 	console.log( five + ' é número?', typeof five === 'number' );
 	var concat = 10 + 10;
 	concat = concat.toString();
@@ -45,8 +46,90 @@ e faça a indentação correta.
 	*/
 	
 	let isOperatorValid = (op) => {
-		
+		return !!operation[op];
 	}
 		
+	/*
+	Agora vamos criar a calculadora.
+	- Crie uma função chamada `calculator`, que receberá como parâmetro um
+	operador;
+	- Se o operador não for válido, a função deve retornar "false";
+	- Se o operador for válido, retornar uma segunda função que receberá dois
+	parâmetros;
+	- Se algum dos parâmetros não for um número, retornar "false";
+	- Senão, retornar o método do objeto "operation" criado acima, baseado no
+	operador passado para a função "calculator", e passando para esse método
+	os dois parâmetros da função de retorno de "calculator".
+	*/
+	let calculator = function(op) {
+		if(! op) {
+			return false;
+		} else {
+			return function(x, y) { 
+				if (typeof x != "number" || typeof y != "number") { 
+					return false; 
+				} else {
+					return operation[op](x, y);
+				}
+			}
+		}
+	}
 	
+	/*
+	Crie uma função chamada "showOperationMessage" que recebe três parâmetros:
+	- o operador, o primeiro número e o segundo número. O retorno da função
+	deve ser a frase:
+	'A operação [NUMBER1] [OPERATOR] [NUMBER2] =';
+	Essa função mostrará a mensagem da operação que criaremos mais abaixo.
+	*/
+	let showOperationMessage = (op, num1, num2) => {
+		return `A operação ${num1} ${op} ${num2}  ='`;
+	}
+	
+	/*
+	Crie uma função chamada "showErrorMessage" que recebe um parâmetro: o
+	operador da operação cálculo, quando a operação não for válida.
+	Essa função deverá retornar a frase:
+	'Operação "[OPERATOR]" não permitida!'
+	*/
+	let showErrorMessage = (op) => {
+		if(!op) {
+			return `Operação "${op}" não permitida!`;
+		}
+	}
+	
+	/*
+	Nossa calculadora está pronta! Agora vamos testá-la:
+	PASSO 1:
+	- Declare 3 variáveis: "number1" e "number2", iniciando com valor zero, e
+	"operationSignal", sem valor por enquanto.
+	*/
+	let number1 = 0;
+	let number2 = 0;
+	let operationSignal;
+	
+	/*
+	PASSO 2:
+	Atribua à variável operationSignal o operador de soma, e declare uma
+	variável chamada "sum", que receba a função "calculator", passando por
+	parâmetro a variável que recebeu o sinal da operação.
+	*/
+	operationSignal = '+';
+	let sum = calculator(operationSignal);
+	console.log('sum ', sum);
+	
+	/*
+	PASSO 3:
+	"sum" agora é uma função, e, se o sinal correto não foi passado para a
+	função "calculator", "sum" será false. Certifique-se de que "sum" não é
+	"false", e então atribua às variáveis "number1" e "number2", dois números
+	que serão os operandos da operação de soma.
+	Após isso, mostre no console o resultado da operação, passando dois
+	parâmetros para o método "log" de "console":
+	- O primeiro será a mensagem da operação (usando a função criada acima);
+	- O segundo, a função de soma, passando os dois operandos.
+	- Se "sum" for "false", mostrar no console a mensagem de erro.
+	*/
+	
+		
 }())
