4dd82c053d1695156aef09b2b56f623cafb691f4
Rafael Gomes <rafarfl2010@gmail.com>
2020-06-15 23:39:30 -0300

    challenge 14

--- section-14/challenge-14.js
+++ section-14/challenge-14.js
@@ -27,7 +27,7 @@
 	*/
 	let justNumbers = [];	
 	
-	for(let i=0; i<numberObjects.length; i++){
+	for(let i=1; i<numberObjects.length; i++){
 		justNumbers.push(numberObjects[i].number);
 	};
 	console.log( '\nJust Numbers:' );
@@ -38,8 +38,120 @@
 	somente os números que forem divisíveis por 2 ou 3. Mostre esse novo array
 	no console.
 	*/
-	let justMod2Or3 = [];
 	
-
+	console.log( '\nJust module of division by 2 or 3:' );
+	let justMod2Or3 = justNumbers.filter((item)=>{
+		return item % 2 === 0 || item % 3 === 0;
+	});
+	
+	console.log( justMod2Or3);
+	
+	/*
+	Declare uma variável chamada operation que receba, do array criado acima,
+	um valor reduzido pela seguinte operação:
+	- Somar 1 ao último valor retornado;
+	- Multiplicar o resultado pelo valor atual.
+	O cálculo deve começar com zero.
+	Mostre o resultado no console.
+	*/
+	console.log( '\nOperation:' );
+	
+	let operation = justMod2Or3.reduce((acumulado, atual)=> {
+		return (acumulado + 1 ) * atual;
+	}, 0);
+	
+	console.log( operation);
+	
+	/*
+	Faça o mesmo cálculo passado acima, mas começando do último item para o
+	primeiro. O nome da variável deve ser operation2. Mostre o resultado no
+	console.
+	*/
+	console.log( '\nOperation 2:' );
+	// ?
+	
+	let operation2 = justMod2Or3.reduceRight((acumulado, atual)=> {
+		return (acumulado + 1 ) * atual;
+	}, 0);
+	
+	console.log( operation2);
+	
+	/*
+	Crie um array chamado `name`. Cada elemento desse array deve ser uma sílaba
+	do seu nome. Vamos reduzir esse array, juntando todas as sílabas, mas usando
+	a "língua do P".
+	PS.: Lembra da língua do "P"? Não? A língua do "P" é uma brincadeira
+	infantil, onde você coloca a letra "P" antes de cada sílaba de uma palavra
+	falada, como se você estivesse falando em código xD
+	*/
+	console.log( '\nSeu nome na língua do "P":' );
+	// ?
+	
+	let name = ['Ra', 'fa', 'el'];
+	let reduceP = name.reduce((acumulado, atual)=> {
+		return acumulado + "P" + atual;
+	}, '');
+	console.log(reduceP);
+	
+	/*
+	Crie uma variável chamada `inversedName`, que reduzirá o array em uma string
+	e atribuirá o seu nome invertido (usando o array criado acima).
+	*/
+	console.log( '\nInversed Name:' );
+	// ?
+	
+	let inversedName = name.reduceRight((acumulado, atual)=> {
+		return acumulado + atual;
+	});
+	console.log(inversedName);
+	
+	/*
+	Mostre no console o array `numberObjects`.
+	*/
+	console.log( '\nNumber objects' );
+	// ?
+	
+	console.log(numberObjects);
+	
+	/*
+	Verifique se existem em algum índice de numberObjects um objeto ìgual a
+	{ number: 2 }. Se houver, mostre no console:
+	- "Existe um objeto { number: 2 } em numberObjects!"
+	Senão, mostre a frase:
+	- "Não existe um objeto { number: 2 } em numberObjects :("
+	Consegue prever o resultado? Deixe uma mensagem no console tentando explicar
+	o que acontece ;)
+	*/
+	console.log( '\nExiste um { number: 2 } em numberObjects?' );
+	// ?
+	
+	let obj = numberObjects[1];
+	
+	if(numberObjects.indexOf(obj) > -1) {
+		console.log('Existe um objeto {number: 2} em numberObjects!');
+	} else {
+		console.log('Não existe um objeto {number: 2} em numberObjects!');
+	}
+	
+	/*
+	Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
+	será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
+	*/
+	console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?' );
+	// ?
+	if(numberObjects.indexOf({number: 2}, 2) > -1) {
+		console.log('Existe um objeto {number: 2} em numberObjects!');
+	} else {
+		console.log('Não existe um objeto {number: 2} em numberObjects!');
+	}
+	
+	/*
+	Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
+	formato de String.
+	*/
+	console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:' );
+    if(Array.isArray( justMod2Or3 )) {
+		console.log(justMod2Or3.toString());
+	}
 
 }())
